a5669151300303d962d89dd2acc15d15918990f0
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-10-02 19:45:51 +0200

    Come up with a simple demo setup for the screenshot
    Just marking some slots some types. I extended the height of the window to avoid the scrollbar (hopefully).

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 import render from './render.js';
 
 render();
+
+function makeEvent(name, data) {
+  const event = new Event(name);
+  for (const key in data) {
+    event[key] = data[key];
+  }
+
+  return event;
+}
+
+if (location.search === '?demo') {
+  // Clear the local storage for local development, it is always empty on the CI
+  localStorage.clear();
+
+  const table = document.querySelector('table');
+  const trs = document.querySelectorAll('tr');
+  const typeInput = document.querySelector('#typeInput');
+
+  for (const tr of trs) {
+    const tds = tr.querySelectorAll('td');
+
+    // Make the first 48 slots (8 hours of 6 10-minute slots) as "sleeping"
+    for (let index = 0; index < 48; index++) {
+      const td = tds[index];
+      td.dispatchEvent(makeEvent('mousedown', { buttons: 1 }));
+    }
+
+    table.dispatchEvent(makeEvent('mouseup'));
+    typeInput.value = 'sleeping';
+    typeInput.dispatchEvent(makeEvent('keydown', { key: 'Enter' }));
+
+    // Make the next 48 slots (8 hours of 6 10-minute slots) as "coding"
+    for (let index = 48; index < 96; index++) {
+      const td = tds[index];
+      td.dispatchEvent(makeEvent('mousedown', { buttons: 1 }));
+    }
+
+    table.dispatchEvent(makeEvent('mouseup'));
+    typeInput.value = 'coding';
+    typeInput.dispatchEvent(makeEvent('keydown', { key: 'Enter' }));
+  }
+}
